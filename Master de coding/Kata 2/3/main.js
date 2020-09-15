2b5d811247adf1011b54c02ff75d3bad36154a72
Santiago Graue de Palacio <67671551+graue00@users.noreply.github.com>
2020-09-14 21:38:03 -0500

    Add files via upload

---
+++ Master de coding/Kata 2/3/main.js
@@ -0,0 +1,107 @@
+//Variables
+//Entradas
+// Salidas
+//Tipos de Datos
+
+//Estrcuturas de Control - Condiciones
+
+/*
+Ciclos For
+*/
+
+/*
+Estructuras de Control
+Ciclos For
+*/
+
+// if () {}
+
+//i es de iterador
+
+/*
+1) Iterador variable de control
+2) Condición de iteración Rango
+3) Como modificamos la variable de control
+*/
+
+// ++ agrega en 1
+//como i es menor que 5 porque empieza en cero se cumple
+
+// for (var i = 0; i <5; i++) {
+//     console.log("hola a todos") // iteración -ciclo de bucle - Vuelta o repetición (son nombres de lo de adentro porque se repita)
+//     console.log(i); 
+// }
+
+//imprimir los numeros del 1 al 10 en consola
+// for (var i =1; i <= 10; i++) {
+//     console.log(i)
+// }
+
+//como puedo mostrar los valores pares del 1-10
+
+//El triple igual es una comparación de identidad
+
+//Mediante el modulo %
+// for (var i =1; i <= 10; i++) {
+//   //El numero al dividirlo entre dos me da como residuo 0?
+// if (i%2 === 2)
+//   //Si me da cero, entonces lo muestro porque es par
+
+//   // Si no me da cero no hago con algo
+
+
+
+//   //Mediante i+=2
+// for (var i=0; i<=10; i+=2) {
+//     console.log(i);
+// }
+
+
+/*
+Arreglos (Arrays) son listas
+*/
+
+// las computadoras empiezan a contar desde el cero
+var arreglo = [
+    1, //pocisión 0
+    2, //pocisión 1
+    3, //pocisión 2
+    4, //pocisión 3
+    5] //pocisión 4
+    ;
+// console.log(arreglo);
+// console.log(arreglo[3], arreglo[0]);
+
+/*lista de multiples tipos de datos
+*/
+
+//lo conveniente es siempre manejar un tipo de dato
+// var listaBiblioteca =[
+//     "Jose Emilio Pacheco"
+// 37,
+// false
+// ];
+
+// console.log(listaBiblioteca);
+// console.log(listaBiblioteca.length);
+
+var frutas = [
+    "mango",
+    "uva",
+    "platano",
+    "carambola",
+    "kiwi",
+    "tamarindo",
+    "durazno",
+    "nispero",
+    "granada",
+    "lichi",
+    "lulo",
+];
+
+// console.log(frutas);
+
+// for (var i = 0; <=10; i++) {
+// console.log(frutas[1]);
+// };
+
